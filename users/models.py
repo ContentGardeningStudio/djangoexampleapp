5e0bf7256a03aca23f3f6d4bc096b9f61805e20b
kamon <kamon.ayeva@gmail.com>
2023-10-18 15:05:54 +0200

    Initial version of the code

---
+++ users/models.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
+from django.db import models
+
+
+# Custom User Manager
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        pass
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        pass
+
+
+# Custom User
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to="profile_pics/")
+    bio = models.TextField()
+
+    def __str__(self):
+        return f"{self.user} profile"
