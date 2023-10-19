23284445bc54817dfe2436727e6c750e0af9e560
kamon <kamon.ayeva@gmail.com>
2023-10-19 21:30:00 +0200

    Plug signals for profiles update from users

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager)
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Custom User Manager
@@ -31,3 +33,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user} profile"
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
