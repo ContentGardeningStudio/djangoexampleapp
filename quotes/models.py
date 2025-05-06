beeaae0a873701b8c1d82d6e2907df73541ad32f
djimi <djimi9530@gmail.com>
2025-05-06 17:09:34 +0200

    quote model

---
+++ quotes/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from users.models import User
+
+
+class Quote(models.Model):
+    quote = models.CharField(max_length=300, unique=True)
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    posted_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.quote
+
+    def __unicode__(self):
+        return self.quote
+
