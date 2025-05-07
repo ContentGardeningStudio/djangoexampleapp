87dcb62ac4dbdf5292e6184517cf5324a610844b
djimi <djimi9530@gmail.com>
2025-05-07 16:58:59 +0200

    basic test for quote

--- quotes/tests.py
+++ quotes/tests.py
@@ -1,1 +1,14 @@
-# Create your tests here.
+import pytest
+from model_bakery import baker
+from .models import Quote
+from users.models import User
+from datetime import datetime
+
+
+@pytest.mark.django_db
+def test_quote_create():
+    # user = User.objects.first()
+    # user = create_user()
+    user = baker.make(User, email="test@example.com", password="password123")
+    quote = baker.make(Quote, quote="quote", posted_date=datetime.now(), poster=user)
+    assert str(quote) == "quote"
