61ba52fc6d0edd56f0f4bd31cd62ca42dcfcff88
Kamon Ayeva <kamon.ayeva@gmail.com>
2025-05-07 18:05:31 +0200

    Simplify + improve the quotes test

--- quotes/tests.py
+++ quotes/tests.py
@@ -2,13 +2,10 @@ import pytest
 from model_bakery import baker
 from .models import Quote
 from users.models import User
-from datetime import datetime
 
 
 @pytest.mark.django_db
 def test_quote_create():
-    # user = User.objects.first()
-    # user = create_user()
     user = baker.make(User, email="test@example.com", password="password123")
-    quote = baker.make(Quote, quote="quote", posted_date=datetime.now(), poster=user)
-    assert str(quote) == "quote"
+    quote = baker.make(Quote, quote="This is a sample quote", poster=user)
+    assert str(quote) == "This is a sample quote"
