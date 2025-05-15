e4f362c1820d7317ca564359d1695b5b4946c07e
djimi <djimi9530@gmail.com>
2025-05-15 18:28:55 +0200

    populate users

--- quotes/management/commands/populate.py
+++ quotes/management/commands/populate.py
@@ -1,11 +1,13 @@
 from django_docopt_command import DocOptCommand
 from quotes.models import Quote
+from users.models import User
 from model_bakery import baker
 from faker import Faker
-from quotes.lib import quote_customizer
+from quotes.lib import quote_customizer, user_customizer
 
 import warnings
-warnings.filterwarnings('ignore')
+
+warnings.filterwarnings("ignore")
 
 fake = Faker()
 
@@ -15,13 +17,22 @@ class Command(DocOptCommand):
         Populate database
         Usage:
             populate (-h | --help)
-            populate
+            populate (--quote)
+            populate (--user)
         """
 
     def handle_docopt(self, arguments):
-        for _ in range(20):
-            try:
-                new_quote = baker.make(Quote, **quote_customizer())
-                print(f"New quote created: {new_quote}")
-            except Exception as e:
-                print("Exception: ", e)
+        if arguments["--quote"]:
+            for _ in range(20):
+                try:
+                    new_quote = baker.make(Quote, **quote_customizer())
+                    print(f"New quote created: {new_quote}")
+                except Exception as e:
+                    print("Exception: ", e)
+        elif arguments["--user"]:
+            for _ in range(20):
+                try:
+                    new_user = baker.make(User, **user_customizer())
+                    print(f"New user created: {new_user}")
+                except Exception as e:
+                    print("Exception: ", e)
