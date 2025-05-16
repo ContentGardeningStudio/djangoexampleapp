4f6758521a55e265c70e4bf6098aa0c49854841b
Kamon Ayeva <kamon.ayeva@gmail.com>
2025-05-16 20:33:53 +0200

    General improvements

--- quotes/lib.py
+++ quotes/lib.py
@@ -5,22 +5,26 @@ from users.models import User
 fake = Faker()
 
 
-def get_users():
-    return User.objects.filter(is_active=True, is_staff=True)
+def get_active_users(only_staff=True):
+    if only_staff:
+        return User.objects.filter(is_active=True, is_staff=True)
+    return User.objects.filter(is_active=True)
 
 
-def quote_customizer():
-    all_users = get_users()
-
-    return {
-        "quote": fake.sentence(nb_words=random.randint(6, 15)),
-        "author": fake.name(),
-        "poster": random.choice(all_users),
-        "posted_date": fake.date_this_decade(),
-    }
-
-
-def user_customizer(is_staff=None):
-    if is_staff is not None:
-        return {"email": fake.email(), "is_staff": is_staff}
-    return {"email": fake.email(), "is_staff": fake.boolean()}
+def model_data_customizer(model_name, **kwargs):
+    match model_name:
+        case "user":
+            if "is_staff" in kwargs:
+                return {"email": fake.email(), "is_staff": kwargs["is_staff"]}
+            return {"email": fake.email(), "is_staff": fake.boolean()}
+            # to do: handle the generation of the password?
+        case "quote":
+            staff_users = get_active_users(only_staff=True)
+            return {
+                "quote": fake.sentence(nb_words=random.randint(6, 15)),
+                "author": fake.name(),
+                "poster": random.choice(staff_users),
+                "posted_date": fake.date_this_decade(),
+            }
+        case _:
+            print("Not handled!")
--- quotes/management/commands/populate.py
+++ quotes/management/commands/populate.py
@@ -3,8 +3,7 @@ from quotes.models import Quote
 from users.models import User
 from model_bakery import baker
 
-# from faker import Faker
-from quotes.lib import quote_customizer, user_customizer
+from quotes.lib import model_data_customizer
 
 import warnings
 
@@ -16,29 +15,26 @@ class Command(DocOptCommand):
         Populate database
         Usage:
             populate (-h | --help)
-            populate (--quote)
-            populate (--user <is_staff>)
-            populate (--user)
+            populate --quote
+            populate --user [--is-staff]
         """
 
     def handle_docopt(self, arguments):
         if arguments["--quote"]:
             for _ in range(20):
                 try:
-                    new_quote = baker.make(Quote, **quote_customizer())
+                    data = model_data_customizer("quote")
+                    new_quote = baker.make(Quote, **data)
                     print(f"New quote created: {new_quote}")
                 except Exception as e:
                     print("Exception: ", e)
         elif arguments["--user"]:
-            is_staff_argument = (
-                arguments["<is_staff>"] if arguments["<is_staff>"] else None
-            )
-            # if is_staff_argument is None, random boolean will be used
             for _ in range(5):
                 try:
-                    new_user = baker.make(
-                        User, **user_customizer(is_staff=is_staff_argument)
+                    data = model_data_customizer(
+                        "user", is_staff=arguments["--is-staff"]
                     )
-                    print(f"New user created: {new_user} {new_user.is_staff}")
+                    new_user = baker.make(User, **data)
+                    print(f"New user created: {new_user} (Staff? {new_user.is_staff})")
                 except Exception as e:
                     print("Exception: ", e)
