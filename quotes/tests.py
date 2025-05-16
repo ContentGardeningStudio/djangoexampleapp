5550b15e29b1bf4555f15a0f8a506a874bd792e5
Kamon Ayeva <kamon.ayeva@gmail.com>
2025-05-16 13:32:34 +0200

    Fix tests + Wording

--- quotes/tests.py
+++ quotes/tests.py
@@ -7,5 +7,7 @@ from users.models import User
 @pytest.mark.django_db
 def test_quote_create():
     user = baker.make(User, email="test@example.com", password="password123")
-    quote = baker.make(Quote, quote="This is a sample quote", poster=user)
-    assert str(quote) == "This is a sample quote"
+    quote = baker.make(
+        Quote, quote="This is a sample quote", poster=user, author="John Doe"
+    )
+    assert str(quote) == "This is a sample quote - John Doe - By test@example.com"
