8e300ab5de6bbfc69acaa891f213d50ae80d9bfe
djimi <djimi9530@gmail.com>
2025-05-19 17:25:29 +0200

    return Profile queryset instead of User and fix template

--- server/utils.py
+++ server/utils.py
@@ -2,15 +2,17 @@ import random
 
 from faker import Faker
 
-from users.models import User
+from users.models import Profile, User
 
 fake = Faker()
 
 
 def get_active_users(only_staff=True):
     if only_staff:
-        return User.objects.filter(is_active=True, is_staff=True)
-    return User.objects.filter(is_active=True)
+        staff_users = User.objects.filter(is_active=True, is_staff=True)
+        return Profile.objects.filter(user__in=staff_users)
+    active = User.objects.filter(is_active=True)
+    return Profile.objects.filter(user__in=active)
 
 
 def model_data_customizer(model_name, **kwargs):
