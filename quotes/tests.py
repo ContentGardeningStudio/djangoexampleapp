e33dcd87921fe92aac2c6c65c0fc005c1d5d3d48
djimi <djimi9530@gmail.com>
2025-05-20 12:13:37 +0200

    complete tests

--- quotes/tests.py
+++ quotes/tests.py
@@ -1,15 +1,19 @@
 import pytest
 from model_bakery import baker
 
-from users.models import User
+from users.models import Profile, User
 
 from .models import Quote
 
 
 @pytest.mark.django_db
 def test_quote_create():
-    user = baker.make(User, email="test@example.com", password="password123")
+    my_user = baker.make(User, email="test@example.com", password="password123")
+    # profile = baker.make(Profile, user=user)
+    profile = Profile.objects.get(user=my_user)
     quote = baker.make(
-        Quote, quote="This is a sample quote", poster=user, author="John Doe"
+        Quote, quote="This is a sample quote", poster=profile, author="John Doe"
     )
+    # print(str(quote))
+    # print("This is a sample quote - John Doe - By test@example.com")
     assert str(quote) == "This is a sample quote - John Doe - By test@example.com"
