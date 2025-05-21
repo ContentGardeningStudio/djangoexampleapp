56382d09ba725cb9099fe60063a3d485c466f592
djimi <djimi9530@gmail.com>
2025-05-21 13:25:00 +0200

    style to register form

--- users/forms.py
+++ users/forms.py
@@ -10,12 +10,26 @@ User = get_user_model()
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs["class"] = (
+                "bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            )
+
     class Meta:
         model = User
         fields = ("email", "password1", "password2")
 
 
 class ProfileForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs["class"] = (
+                "bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            )
+
     class Meta:
         model = Profile
         fields = ("full_name", "bio")  # adjust fields as needed
