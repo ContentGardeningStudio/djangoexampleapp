396fed0a7f6387de3ea8f75a8ebde0181cfc6fef
djimi <djimi9530@gmail.com>
2025-05-22 18:27:42 +0200

    add authors list

--- quotes/views.py
+++ quotes/views.py
@@ -2,7 +2,7 @@ from django.core.paginator import Paginator
 from django.shortcuts import render
 from django_style import Nav
 
-from .models import Quote
+from .models import Quote, QuoteAuthor
 
 
 def home(request):
@@ -11,9 +11,7 @@ def home(request):
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
-    site_nav = [
-        Nav("Home", "home"),
-    ]
+    site_nav = [Nav("Home", "home"), Nav("Authors", "list_authors")]
     if request.user.is_anonymous:
         site_nav += [Nav("Login", "login"), Nav("Register", "register")]
     else:
@@ -22,6 +20,28 @@ def home(request):
     return render(
         request,
         "home.html",
+        context={
+            "site_nav": site_nav,
+            "page_obj": page_obj,
+        },
+    )
+
+
+def list_authors(request):
+    authors = QuoteAuthor.objects.all()
+    paginator = Paginator(authors, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    site_nav = [Nav("Home", "home"), Nav("Authors", "list_authors")]
+    if request.user.is_anonymous:
+        site_nav += [Nav("Login", "login"), Nav("Register", "register")]
+    else:
+        site_nav += [Nav("Profile", "profile"), Nav("Logout", "logout")]
+
+    return render(
+        request,
+        "authors.html",
         context={
             "site_nav": site_nav,
             "page_obj": page_obj,
