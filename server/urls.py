2427fe3ba3941f121459c0ae142ccc332a928128
Kamon Ayeva <kamon.ayeva@gmail.com>
2025-05-22 19:35:10 +0200

    First version of the edit_profile + Fix media path/URL in dev mode

--- server/urls.py
+++ server/urls.py
@@ -1,16 +1,22 @@
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
 from quotes import views as quotes_views
 from users import views as users_views
 
-urlpatterns = [
-    path("", quotes_views.home, name="home"),
-    path("admin/", admin.site.urls),
-    path("register/", users_views.register_view, name="register"),
-    path("login/", users_views.login_view, name="login"),
-    path("profile/", users_views.profile_view, name="profile"),
-    path("profile/edit/", users_views.edit_profile_view, name="edit_profile"),
-    path("logout/", users_views.custom_logout_view, name="logout"),
-] + debug_toolbar_urls()
+urlpatterns = (
+    [
+        path("", quotes_views.home, name="home"),
+        path("admin/", admin.site.urls),
+        path("register/", users_views.register_view, name="register"),
+        path("login/", users_views.login_view, name="login"),
+        path("profile/", users_views.profile_view, name="profile"),
+        path("profile/edit/", users_views.edit_profile_view, name="edit_profile"),
+        path("logout/", users_views.custom_logout_view, name="logout"),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + debug_toolbar_urls()
+)
--- users/forms.py
+++ users/forms.py
@@ -39,3 +39,13 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ["full_name", "bio", "picture"]
+
+        widgets = {
+            "picture": forms.ClearableFileInput(
+                attrs={
+                    "class": "block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 \
+                          file:rounded-full file:border-0 file:text-sm file:font-semibold \
+                          file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
+                }
+            )
+        }
