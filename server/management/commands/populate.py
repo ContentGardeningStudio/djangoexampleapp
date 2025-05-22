b977b4e42323634cfd1d41b7fa1844974eda4a92
Kamon Ayeva <kamon.ayeva@gmail.com>
2025-05-22 15:27:37 +0200

    Diverse fixes for user/profile/quote

--- server/management/commands/populate.py
+++ server/management/commands/populate.py
@@ -6,7 +6,7 @@ from model_bakery import baker
 
 from quotes.models import Quote, QuoteAuthor
 from server.utils import model_data_customizer
-from users.models import User
+from users.models import Profile, User
 
 warnings.filterwarnings("ignore")
 
@@ -31,17 +31,28 @@ class Command(DocOptCommand):
                 except Exception as e:
                     print("Exception: ", e)
         elif arguments["--user"]:
+            # handles user + profile
             for _ in range(5):
                 try:
                     data = model_data_customizer(
                         "user", is_staff=arguments["--is-staff"]
                     )
-                    new_user = baker.make(User, **data)
+                    user_data = {"email": data["email"]}
+                    new_user = baker.make(User, **user_data)
+                    # refresh to avoid the signal side effect
+                    new_user.refresh_from_db()
+                    # add to the right group
                     new_user.groups.add(Group.objects.get(name="Members"))
 
                     print(
-                        f"New member user created: {new_user} (Staff? {new_user.is_staff})"
+                        f"New member user created: {new_user.email} (Staff? {new_user.is_staff})"
                     )
+
+                    # Also update the profile which was created via the signal
+                    new_user_prof = Profile.objects.get(user=new_user)
+                    new_user_prof.full_name = data["full_name"]
+                    new_user_prof.save()
+                    print(f"=> Profile for the user created: {new_user_prof.full_name}")
                 except Exception as e:
                     print("Exception: ", e)
         elif arguments["--author"]:
--- server/utils.py
+++ server/utils.py
@@ -8,28 +8,32 @@ from users.models import Profile, User
 fake = Faker()
 
 
-def get_active_users(only_staff=True):
+def get_active_users(only_staff=False):
     if only_staff:
-        staff_users = User.objects.filter(is_active=True, is_staff=True)
-        return Profile.objects.filter(user__in=staff_users)
-    active = User.objects.filter(is_active=True)
-    return Profile.objects.filter(user__in=active)
+        users = User.objects.filter(is_active=True, is_staff=True)
+    else:
+        users = User.objects.filter(is_active=True)
+    print(users)
+    return Profile.objects.filter(user__in=users)
 
 
 def model_data_customizer(model_name, **kwargs):
     match model_name:
         case "user":
+            person = {"full_name": fake.name(), "email": fake.email()}
             if "is_staff" in kwargs:
-                return {"email": fake.email(), "is_staff": kwargs["is_staff"]}
-            return {"email": fake.email(), "is_staff": fake.boolean()}
+                person["is_staff"] = kwargs["is_staff"]
+            else:
+                person["is_staff"] = fake.boolean()
+            return person
             # to do: handle the generation of the password?
         case "quote":
-            staff_users = get_active_users(only_staff=True)
+            active_users = get_active_users()
             authors = QuoteAuthor.objects.all()
             return {
                 "quote": fake.sentence(nb_words=random.randint(6, 15)),
                 "author": random.choice(authors),
-                "poster": random.choice(staff_users),
+                "poster": random.choice(active_users),
                 "posted_date": fake.date_this_decade(),
             }
         case "author":
