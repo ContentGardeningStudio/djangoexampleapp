6ca00ba4af2389d263bd139e01c3386794305da2
djimi <djimi9530@gmail.com>
2025-05-28 14:55:02 +0200

    list quotes in author detail with filter

--- quotes/views.py
+++ quotes/views.py
@@ -29,3 +29,8 @@ class QuoteAuthorDetailView(SiteNavMixin, DetailView):
     template_name = "content/author.html"
     slug_field = "id"
     slug_url_kwarg = "id"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["object_list"] = Quote.objects.filter(author=self.get_object())
+        return context
