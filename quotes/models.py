cf2b74d8159aad3661f00e99e6964d8e0b6fc4fa
djimi <djimi9530@gmail.com>
2025-05-30 11:24:14 +0200

    add slug on quote author model

--- quotes/models.py
+++ quotes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 from django_countries.fields import CountryField
 from taggit.managers import TaggableManager
 
@@ -8,9 +9,15 @@ from accounts.models import Profile
 class QuoteAuthor(models.Model):
     name = models.CharField(max_length=300)
     country = CountryField(null=True, blank=True)
+    slug = models.SlugField(null=False, unique=True)
 
     def __str__(self):
         return f"{self.name}"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
 
 
 class Quote(models.Model):
