44806bf2d2f51df20139f0606754c8fb41b25644
Kamon Ayeva <kamon.ayeva@gmail.com>
2025-07-25 09:19:02 +0200

    Group all signals together

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 class UserManager(BaseUserManager):
@@ -48,14 +46,3 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.email}"
-
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
