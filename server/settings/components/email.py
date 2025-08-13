014c5fe5159d65a6e60d59d0f40087e0a83ef28f
djimi <djimi9530@gmail.com>
2025-08-13 19:13:15 +0200

    setting mailgun

--- server/settings/components/email.py
+++ server/settings/components/email.py
@@ -1,16 +1,24 @@
 # from server.settings.components import config
+import os
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+EMAIL_BACKEND = "anymail.backends.brevo.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 # EMAIL_FILE_PATH = BASE_DIR / "app-messages"
 
 # EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"  # or amazon_ses.EmailBackend, or...
 
-# ANYMAIL = {
-#     "MAILGUN_API_KEY": config('MAILGUN_API_KEY'),
-#     "MAILGUN_API_URL": "https://api.eu.mailgun.net/v3",
-#     "MAILGUN_SENDER_DOMAIN": 'mydomain.com',  # your Mailgun domain, if needed
-# }
+ANYMAIL = {
+    "BREVO_API_KEY": os.environ.get("BREVO_API_KEY"),
+    #     "MAILGUN_API_KEY": config('MAILGUN_API_KEY'),
+    #     "MAILGUN_API_URL": "https://api.eu.mailgun.net/v3",
+    #     "MAILGUN_SENDER_DOMAIN": 'mydomain.com',  # your Mailgun domain, if needed
+}
 # DEFAULT_FROM_EMAIL = "contact@mydomain.com"  # if you don't already have this in settings
 # # SERVER_EMAIL = "your-server@example.com"  # ditto (default from-email for Django errors)
